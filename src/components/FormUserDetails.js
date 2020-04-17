54d4a5196b1f53a76a8b1298319e88ad186535d8
Paula Alves <paulafernandaalves@icloud.com>
2020-04-17 20:40:20 -0300

    included functions and update components

---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class FormUserDetails extends Component {
+	render() {
+		return (
+			<div>
+				<h1>helllo</h1>
+			</div>
+		)
+	}
+}
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import FormUserDetails from './FormUserDetails';
 
-export default class UserForm extends Component {
+export class UserForm extends Component {
 	state = {
 		step: 1,
 		firstName: '',
@@ -9,13 +10,54 @@ export default class UserForm extends Component {
 		occupation: '',
 		city: '',
 		bio: ''
-
 	}
-	render() {
-		return (
-			<div>
 
-			</div>
-		)
+	// proceed to next step
+	nextStep = () => {
+		const { step } = this.state;
+		this.setState({
+			step: step + 1
+		});
+	}
+
+	// proceed to prev step
+	prevStep = () => {
+		const { step } = this.state;
+		this.setState({
+			step: step - 1
+		});
+	}
+
+	// handle fields change
+	handleChange = input => e => {
+		this.setState({[input]: e.target.value})
+	}
+
+
+
+	render() {
+		const { step } = this.state;
+		const { firstName, lastName, email, occupation, city, bio } = this.state;
+		const values = { firstName, lastName, email, occupation, city, bio }
+
+		switch (step) {
+			case 1:
+				return (
+					<FormUserDetails
+						nextStep={this.nextStep}
+						handleChange={this.handleChange}
+						values={values}
+					/>
+				)
+			case 2:
+				return <h1>FormPersonalDetails</h1>
+			case 3:
+				return <h1>Confirm</h1>
+			default:
+				return <h1>Success</h1>
+
+		}
 	}
 }
+
+export default UserForm;
