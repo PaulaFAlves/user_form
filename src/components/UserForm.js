b3a4d09a6e87a188e22dd3c1f545271f5ba808e2
Paula Alves <paulafernandaalves@icloud.com>
2020-04-17 18:26:05 -0300

    initial commit

---
+++ src/components/UserForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class UserForm extends Component {
+	state = {
+		step: 1,
+		firstName: '',
+		lastName: '',
+		email: '',
+		occupation: '',
+		city: '',
+		bio: ''
+
+	}
+	render() {
+		return (
+			<div>
+
+			</div>
+		)
+	}
+}
