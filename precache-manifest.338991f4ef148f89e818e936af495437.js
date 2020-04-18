aba85296dd736c870fc3479524cd0775bef4765c
Paula Alves <paulafernandaalves@icloud.com>
2020-04-18 12:19:03 -0300

    Updates

---
+++ precache-manifest.338991f4ef148f89e818e936af495437.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7383175e5de14cb24afce84d60529f7c",
+    "url": "/user_form/index.html"
+  },
+  {
+    "revision": "90f0fe21e30adacb6bfc",
+    "url": "/user_form/static/js/2.707679dd.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/user_form/static/js/2.707679dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2757f8152a7462cc7a",
+    "url": "/user_form/static/js/main.6b77d129.chunk.js"
+  },
+  {
+    "revision": "f5f6822736effd9af361",
+    "url": "/user_form/static/js/runtime-main.183006f0.js"
+  }
+]);
