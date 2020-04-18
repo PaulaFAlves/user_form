566d6ec76b6512487e0650bad699ae05c901a575
Paula Alves <paulafernandaalves@icloud.com>
2020-04-18 11:51:15 -0300

    included function on the components

---
+++ src/components/Success.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+export default class Success extends Component {
+	continue = e => {
+		e.preventDefault();
+		// process form //
+		this.props.nextStep();
+	}
+
+	back = e => {
+		e.preventDefault();
+		this.props.prevStep();
+	}
+
+	render() {
+		return (
+			<MuiThemeProvider>
+				<React.Fragment>
+					<AppBar title="Confirm User Data" />
+					<h1>Thank you for your Submission</h1>
+				</React.Fragment>
+			</MuiThemeProvider>
+		)
+	}
+}
+
